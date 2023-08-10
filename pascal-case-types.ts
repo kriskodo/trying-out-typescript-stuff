62e08fe009c7016aecfb58eba3cd7ef373f8a6df
HDElemental <kristiangeorgiev99ss@abv.bg>
2023-08-10 12:25:40 +0300

    clear unnecesary code & fix Uppercase case

--- pascal-case-types.ts
+++ pascal-case-types.ts
@@ -3,49 +3,34 @@
  * да са pascal case версия на стойностите
  */
 
-
-type RemoveSpaces<S extends string> = S extends `${infer Left} ${infer Right}`
-    ? `${RemoveSpaces<`${Lowercase<Left>}${CamelCase<Lowercase<Right>>}`>}`
+type WithoutSpaces<S extends string> = S extends `${infer First} ${infer Rest}`
+    ? `${WithoutSpaces<`${Lowercase<First>}${CamelCase<Lowercase<Rest>>}`>}`
     : S;
 
 type CamelCase<S extends string> =
-    S extends `${infer First}${infer Rest}`
-    ? `${Uppercase<First>}${RemoveSpaces<Rest>}`
+    S extends WithoutSpaces<Uppercase<S>>
+    ? Capitalize<Lowercase<S>>
+    : S extends `${infer First}${infer Rest}`
+    ? `${Uppercase<First>}${WithoutSpaces<Rest>}`
     : '';
 
 type KeysAsCamelCaseValues<T extends Record<string, string>> {
     -readonly [K in keyof T as CamelCase<T[K]>]: T[K]
 }
 
-type Writeable<T extends Record<string, string>, K extends string> = {
-    [P in K]: T[P];
-}
+const convertToCamelCase = <T extends Record<string, string>>(object: T): KeysAsCamelCaseValues<T> => {
+    return object as unknown as KeysAsCamelCaseValues<T>;
+};
 
-const Profession = {
+const Profession = convertToCamelCase({
     Influencer: 'influencer',
     ProductManager: 'productManager',
     DataScientist: 'Data Scientist',
     SoftwareDeveloper: 'software developer',
-    withMultipleSpacesAndRandomName: 'Multiple    Spaces',
+    MultipleSpaces: 'Multiple    Spaces',
     MoreThanTwoStrings: 'more than two strings',
     UppercaseStrings: "UPPERCASE STRINGS",
-    UPPERCASE: "UPPERCASE"
-} as const;
+    UPPERCASE: "UPPERCASE",
+} as const);
 
-function camelize(str: string) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word: string, index: number) {
-        return index === 0 ? word.toLowerCase() : word.toUpperCase();
-    }).replace(/\s+/g, '');
-}
-
-const keysAsCamelCaseValues = <T extends Record<string, string>>(object: T): KeysAsCamelCaseValues<T> => {
-    return Object.keys(object).reduce((acc, objKey) => {
-        return {
-            ...acc,
-            [camelize(objKey)]: object[objKey]
-        }
-    }, {} as KeysAsCamelCaseValues<T>);
-}
-
-const profession = keysAsCamelCaseValues(Profession);
-profession.MultipleSpaces = "Multiple    Spaces";
+Profession.
